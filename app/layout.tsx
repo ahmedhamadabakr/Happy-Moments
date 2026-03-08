24075b1211247375efe3ea3dc31cc15da1572243
habiba20050 <109625191+habiba20050@users.noreply.github.com>
2026-03-08 12:37:43 +0200

    change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import localFont from 'next/font/local'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 import SessionManager from '@/components/SessionManager'
 
-const geist = Geist({ subsets: ["latin"] });
-const geistMono = Geist_Mono({ subsets: ["latin"] });
+const geSsTwo = localFont({
+  src: '../public/fonts/GE_SS_Two_Bold.otf',
+  variable: '--font-ge-ss-two',
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'منصة إدارة الفعاليات والدعوات',
@@ -20,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ar" dir="rtl">
-      <body className={geist.className}>
+      <body className={geSsTwo.className}>
         <SessionManager>
           {children}
         </SessionManager>
